bb1719e866816afb8028224bc5c093cae3f4096a
110213041 <s110213041@mail1.ncnu.edu.tw>
2022-05-30 20:43:00 +0800

    improve highlightjs

--- src/assets/Java/J_Chapter3.vue
+++ src/assets/Java/J_Chapter3.vue
@@ -633,7 +633,7 @@ public class Pascal {
 </template>
 
 <script>
-import hljs from "highlight.js/lib/common";
+// import "highlight.js/lib/common";
 import hljsVuePlugin from "@highlightjs/vue-plugin";
 import TableOfContentSidebar from "@/components/TableOfContentSidebar.vue";
 import HeaderTemplate from "@/components/HeaderTemplate.vue";
--- src/main.js
+++ src/main.js
@@ -3,11 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 
 import "highlight.js/styles/github-dark-dimmed.css";
-import hljs from "highlight.js/lib/core";
-import javascript from "highlight.js/lib/languages/javascript";
+import "highlight.js/lib/common";
+// import hljs from "highlight.js/lib/core";
+// import javascript from "highlight.js/lib/languages/javascript";
 import hljsVuePlugin from "@highlightjs/vue-plugin";
 
-hljs.registerLanguage("javascript", javascript);
+// hljs.registerLanguage("javascript", javascript);
 
 const app = createApp(App);
 app.use(router);
