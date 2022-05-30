359b3a20c9a347e6d49e4ba7e2882c7e3403962a
110213041 <s110213041@mail1.ncnu.edu.tw>
2022-05-30 10:26:22 +0800

    publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-    publicPath: "/Computer-Architecture-EndSemWebsite/",
+    publicPath:
+        process.env.NODE_ENV === "production"
+            ? "/Computer-Architecture-EndSemWebsite/"
+            : "/",
     transpileDependencies: true,
 });
